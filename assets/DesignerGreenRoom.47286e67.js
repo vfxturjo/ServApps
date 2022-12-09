77de4df47d8b59464b3f395ca4498b12da2c3b21
vfxturjo <vfxturjo@gmail.com>
2022-12-09 23:35:46 +0600

    Updates

---
+++ assets/DesignerGreenRoom.47286e67.js
@@ -0,0 +1,1 @@
+import{S as I,i,s as r,e as o,a as l,b as c,n,d as f}from"./index.8fa22319.js";function m(s){let e;return{c(){e=o("div"),e.textContent="HIIIIIIIIIIIIIIIIIII",l(e,"class","fixed bg-gray-900 z-50 min-h-full min-w-full flex justify-center items-center")},m(t,a){c(t,e,a)},p:n,i:n,o:n,d(t){t&&f(e)}}}class d extends I{constructor(e){super(),i(this,e,null,m,r,{})}}export{d as default};
