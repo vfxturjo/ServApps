7754c0dce738e98aab8a52eb2642cc89320c4f23
vfxturjo <vfxturjo@gmail.com>
2022-12-10 11:31:48 +0600

    GH-Pages now working!!! Very good setup!

--- README.md
+++ README.md
@@ -3,3 +3,13 @@ This is my first SPA, created with Svelte + Flowbite.
 Still under development
 
 Thanks for visiting!
+
+Todo:
+
+- [] Rename counters mode
+  - [] functionality to add icons/pics?
+- [] change counter cards size, row & column
+
+fix:
+
+- [] font-awesome icon loads very slow
--- src/App.svelte
+++ src/App.svelte
@@ -12,9 +12,11 @@ import { DarkMode } from "flowbite-svelte";
 import DesignerGreenRoom from "./lib/debugging/DesignerGreenRoom.svelte";
 import AppTopBar from "./AppTopBar.svelte";
 import { fade, fly, slide } from "svelte/transition";
+import LoadingAnim from "./lib/loadingAnim.svelte";
 let darkmodebtn =
   "text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-lg p-2.5 fixed right-4 top-2 z-50 transition-all";
 
+let loading_page = false;
 // let CounterApp = null;
 // onMount(async () => {
 //   try {
@@ -27,8 +29,24 @@ let darkmodebtn =
 </script>
 
 <main>
+  <!-- TOP BAR, ALWAYS ACTIVE -->
   <AppTopBar />
-  <Router routes="{routes}" />
+
+  <!-- PAGE LOADING ANIMATION -->
+  {#if loading_page}
+    <LoadingAnim />
+  {/if}
+
+  <!-- PAGES -->
+  <Router
+    routes="{routes}"
+    on:routeLoading="{() => {
+      loading_page = true;
+    }}"
+    on:routeLoaded="{() => {
+      loading_page = false;
+    }}"
+  />
 </main>
 
 <!-- <DesignerGreenRoom /> -->
